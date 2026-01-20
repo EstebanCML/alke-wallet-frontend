df7e371b99e408ed4ebcfe2738e9a43c6d2eff59
EstebanCML <esteban.madrid.83@gmail.com>
2026-01-20 02:25:44 -0300

    implementacion de vista menu, con confirmacion de login

--- js/auth-check.js
+++ js/auth-check.js
@@ -1,3 +1,15 @@
+// auth-check.js
+(function() {
+    const sesion = localStorage.getItem("isLoggedIn");
+    
+    // Si no existe la sesión, redirigir antes de que se pinte el HTML
+    if (sesion !== "true") {
+        window.location.href = "login.html";
+    }
+})();
+
+/*
 $(function(){
 
 });
+*/
